d618b93717b1b987eaea314153e8cdb4c0a20757
Martijn van de Wetering <martijnwetering@gmail.com>
2025-06-10 22:26:01 +0200

    bugfix: Add tailwind import

--- backend/resources/views/layouts/blank.blade.php
+++ backend/resources/views/layouts/blank.blade.php
@@ -19,8 +19,6 @@
         <meta name="robots" content="noindex, nofollow">
         <title>{{ config('app.name') }}</title>
 
-        <!-- Scripts -->
-        <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
         @vite(['resources/css/app.css', 'resources/js/app.js'])
 
         {!! RecaptchaV3::initJs() !!}
