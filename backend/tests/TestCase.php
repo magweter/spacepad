9add93963f3c9be3c31d16096ac00f1be76abd47
Martijn van de Wetering <martijnwetering@gmail.com>
2025-06-15 17:20:35 +0200

    ci: Add vite mocks

--- backend/tests/TestCase.php
+++ backend/tests/TestCase.php
@@ -3,8 +3,17 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Vite;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    use CreatesApplication;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // Mock Vite manifest for testing
+        Vite::fake();
+    }
 }
